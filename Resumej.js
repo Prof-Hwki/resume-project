08b54801afd8b5814babf755efb48e57661ec2fd
Prof-Hwki <spayten3rd@oru.edu>
2025-09-17 09:19:16 -0500

    Update Resumej.js

--- Resumej.js
+++ Resumej.js
@@ -8,16 +8,14 @@ console.log("HEHEHEHHEHEH");
 
 
 //Define Variables
-let myName = "Steven";
-let email = "spayten3rd@oru.edu";
+let myName = "John";
 let title = "Student";
-let experience = "Pizza Hut, Youth Leader, and Soccer Player";
-let education = "Oral Roberts University";
+let experience = "Mcdonalds, Youth Leader, and Shift Leader";
+let education = "Ohio State University";
 let skills = ["C++", "Microsoft Execl", "C#"]; 
 
 //Connecting Variables to HTML
 document.getElementById("my-name").textContent = myName;
-document.getElementById("my-email").textContent = email;
 document.getElementById("title").textContent = title;
 document.getElementById("experience").textContent = experience;
 document.getElementById("education").textContent = education;
