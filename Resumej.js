a9a198dd532f129d751894355810673557311f94
Prof-Hwki <spayten3rd@oru.edu>
2025-09-19 09:18:15 -0500

    Update Resumej.js

--- Resumej.js
+++ Resumej.js
@@ -6,15 +6,22 @@ Ever*/
 
 console.log("HEHEHEHHEHEH");
 
-
 //Define Variables
 let myName = "John";
 let title = "Student";
 let experience = "Mcdonalds, Youth Leader, and Shift Leader";
 let education = "Ohio State University";
-let skills = ["C++", "Microsoft Execl", "C#"]; 
+let skills = ["C++", "Microsoft Execl", "C#"];
+let skillsList = document.createElement('ul');
 
 //Connecting Variables to HTML
+for (let i = 0; i < skills.length; i++){
+  let li = document.createElement('li');
+  li.textContent = skills[i];
+  skillsList.appendChild(li);
+}
+
+document.body.appendChild(skillsList);
 document.getElementById("my-name").textContent = myName;
 document.getElementById("title").textContent = title;
 document.getElementById("experience").textContent = experience;
